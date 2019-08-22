230acb893e1c66dbf7a50abfa677fb9499b5f132
Sviatoslav <s.granovskiy@gmail.com>
2019-08-22 12:35:39 +0300

    hw-3.1-solution

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -3,16 +3,26 @@ package core.basesyntax;
 public class FindNumbers {
 
     /**
-     * <p>Реализуйте метод getAllNumbers(String text) который принимает строку, cодержащую буквы,
+     * <p>Реализуйте метод getAllNumbers(String text)
+     * который принимает строку, cодержащую буквы,
      * целые числа и иные символы.</p>
      *
-     * <p>Требуется: все числа, которые встречаются в строке, поместить в отдельный целочисленный
-     * массив, каждый элемент этого массива умножить на 2. Метод должен возвращать этот массив.</p>
+     * <p>Требуется: все числа, которые встречаются в строке,
+     * поместить в отдельный целочисленный
+     * массив, каждый элемент этого массива умножить на 2.
+     * Метод должен возвращать этот массив.</p>
      *
-     * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
+     * <p>Пример: если дана строка "data 48 call 9 read13 blank0a",
+     * то в массиве должны оказаться
      * числа 96, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        text = text.replaceAll("[^-0-9]+", " ");
+        String[] stringNumbers = text.trim().split(" ");
+        int[] allNumbers = new int[stringNumbers.length];
+        for (int i = 0; i < allNumbers.length; i++) {
+            allNumbers[i] = Integer.parseInt(stringNumbers[i]) * 2;
+        }
+        return allNumbers;
     }
 }
